106c26ee86b9618c555363d3831b046573e9320b
jaydenharrod <jaydenharrod@hip01335-jaydenh.corp.hipagesgroup.com.au>
2018-11-26 12:50:41 +1100

    use heroku instead

--- deploy_server_do.sh
+++ deploy_server_do.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 yarn build:server
-docker build -t jaydenharrod/abb:latest .
-docker push jaydenharrod/abb:latest
-ssh root@178.128.124.167 "docker pull jaydenharrod/abb:latest && docker tag jaydenharrod/abb:latest dokku/abb:latest && dokku tags:deploy abb latest"
+heroku container:push --app=evening-castle-55002 web
+heroku container:release --app=evening-castle-55002 web
